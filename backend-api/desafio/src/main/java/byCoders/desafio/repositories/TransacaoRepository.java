351bd2ca8c823646032e1413b4e40fafc4061011
allanlopesbandtec <allan.lopes@bandtec.com.br>
2021-08-25 18:24:32 -0300

    TransacaoRepository busca de transação por id de loja

--- backend-api/desafio/src/main/java/byCoders/desafio/repositories/TransacaoRepository.java
+++ backend-api/desafio/src/main/java/byCoders/desafio/repositories/TransacaoRepository.java
@@ -2,6 +2,9 @@ package byCoders.desafio.repositories;
 
 import byCoders.desafio.models.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
+
+    List<Transacao> findTransacaoByLoja_IdLoja(Integer idLoja);
 }
