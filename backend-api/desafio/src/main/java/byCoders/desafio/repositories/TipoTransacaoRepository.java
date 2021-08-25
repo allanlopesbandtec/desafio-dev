011bed8e693fc8c0097fc76d30f09f049e439381
allanlopesbandtec <allan.lopes@bandtec.com.br>
2021-08-25 18:09:00 -0300

    Remoção TipoTransacaoRepository

--- backend-api/desafio/src/main/java/byCoders/desafio/repositories/TipoTransacaoRepository.java
+++
@@ -1,8 +0,0 @@
-package byCoders.desafio.repositories;
-
-import byCoders.desafio.models.TipoTransacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoTransacaoRepository extends JpaRepository<TipoTransacao, Integer> {
-
-}
